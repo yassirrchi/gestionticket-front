5f4b03bb0e86f711d9805ad5a35090ac6f3d6e36
yassirrchi <yassirrchi7@gmail.com>
2022-07-24 02:06:04 +0100

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicketsComponent } from './admin/tickets/tickets.component';
 import { TraitementsComponent } from './admin/traitements/traitements.component';
+import { CreerticketComponent } from './client/creerticket/creerticket.component';
 import { LoginComponent } from './client/login/login.component';
 import { MesticketsComponent } from './client/mestickets/mestickets.component';
 import { LoginTechnicienComponent } from './technicien/login-technicien/login-technicien.component';
 
 const routes: Routes = [{path:"login",component:LoginComponent},{path:"technicien/login",component:LoginTechnicienComponent},
-{path:"client/tickets",component:MesticketsComponent},{path:"admin/tickets",component:TicketsComponent}
+{path:"client/tickets",component:MesticketsComponent},{path:"client/ticket/creer",component:CreerticketComponent},{path:"admin/tickets",component:TicketsComponent}
 ,{path:"admin/traitements",component:TraitementsComponent}];
 
 @NgModule({
